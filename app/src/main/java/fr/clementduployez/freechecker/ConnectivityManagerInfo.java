7ec9049ffb45dc341248ac1565e9110ed99d3036
clement <c.duployez@gmail.com>
2015-11-14 16:39:22 +0100

    Primary tests

---
+++ app/src/main/java/fr/clementduployez/freechecker/ConnectivityManagerInfo.java
@@ -0,0 +1,11 @@
+package fr.clementduployez.freechecker;
+
+import android.content.Context;
+
+/**
+ * Created by cdupl on 11/14/2015.
+ */
+public class ConnectivityManagerInfo {
+    public ConnectivityManagerInfo(Context context, MobileInfo mobileInfo) {
+    }
+}
--- app/src/main/java/fr/clementduployez/freechecker/MobileInfo.java
+++ app/src/main/java/fr/clementduployez/freechecker/MobileInfo.java
@@ -10,10 +10,12 @@ public class MobileInfo {
 
     private final Context mContext;
     private final TelephonyManagerInfo mTelephonyManagerInfo;
+    private final ConnectivityManagerInfo mConnectivityManagerInfo;
 
     public MobileInfo(Context context) {
         this.mContext = context;
         mTelephonyManagerInfo = new TelephonyManagerInfo(context,this);
+        mConnectivityManagerInfo = new ConnectivityManagerInfo(context, this);
     }
 
 
